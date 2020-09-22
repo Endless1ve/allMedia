ba979e5ab5ecbb7f09166c8cccc269395c344bc3
Artem Tulupov <stalin.dignitas70@gmail.com>
2020-09-22 13:41:44 +0300

    develop version

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,78 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // добавили плагин
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
-    entry: {main: './src/index.js'},
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {loader: "babel-loader"},
-            exclude: /node_modules/
-        },
-        {
-            test: /\.css$/,
-            use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-    ]
+      },
+      {
+        test: /\.css$/, // применять это правило только к CSS-файлам
+        use: [
+            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+            'css-loader',
+            'postcss-loader'
+        ]
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            // Означает, что:
-            inject: false, // стили НЕ нужно прописывать внутри тегов
-            template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
-            filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-          })
+      {
+        test: /\.(eot|svg|png|jpg|gif|ico)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        use: [{
+            loader: "file-loader",
+            options: {
+                outputPath: './images/',
+                esModule: false,
+            },
+        }, ],
+    },
+    {
+        test: /\.(ttf|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        use: [{
+            loader: "file-loader",
+            options: {
+                outputPath: './fonts/',
+                esModule: false,
+            },
+        }, ],
+    },
+    {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [{
+            loader: "image-webpack-loader",
+            options: {
+                name: "[path][chunkhash].[ext]",
+                bypassOnDebug: true,
+                disable: false,
+            },
+        }, ],
+    }
     ]
-}
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ // 
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+  ]
+};
